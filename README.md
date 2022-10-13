396befebec9774b5f09edad71317b16b579ebc5e
MarioZZJ <mariozzj@outlook.com>
2022-10-13 21:49:46 +0800

    init: 🎉 add pyspark init
    1st commit: add README & spark init script.

---
+++ README.md
@@ -0,0 +1,10 @@
+# pySparkDemo_OpenAlex
+
+实验室服务器已完成 OpenAlex 数据集下载及转换，过程依据[官方文档](https://docs.openalex.org/download-snapshot)。本仓库包含基于 PySpark 的 OpenAlex 读取初始化过程，可方便基于该数据集进行实验。同时可以参考仓库内的 demo notebook 进行基本学习。
+
+## 使用方法
+
+本仓库已设置为 Github 模板仓库，可以基于 demo 创建仓库。步骤：
+1. 点击页面上方 `Use This Template`，创建自己的仓库；
+2. 使用 `git clone` 命令将自己的仓库克隆到到服务器上自己的文件夹内；
+3. 参照 demo，在服务器上创建自己的 notebook，进行自己的实验。注意关键代码块：`%run spark_openalex_init.py`
---
+++ spark_openalex_init.py
@@ -0,0 +1,116 @@
+import findspark
+
+findspark.init('/opt/spark-3.1.2')
+
+from pyspark.sql.types import *
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F # apache 查询使用文档
+from pyspark.sql.functions import *
+from pyspark.sql.window import Window
+
+spark = SparkSession \
+    .builder \
+    .master("spark://c8mao:7077")\
+    .config("spark.executor.cores","4")\
+    .config("spark.driver.memory", "20g") \
+    .config("spark.executor.memory", "16g") \
+    .config("spark.dynamicAllocation.enabled", "true") \
+    .config("spark.dynamicAllocation.shuffleTracking.enabled", "true") \
+    .config("spark.shuffle.service.enabled", "true") \
+    .config("spark.hadoop.fs.permissions.umask-mode", "000") \
+    .appName("OpenAlex_Username_Demo") \
+    .getOrCreate()
+
+class OpenAlex():
+
+  # return stream path
+  def getFullpath(self, streamName):
+    path = r'/home/Public/OpenAlex/csv-files' + self.streams[streamName][0]
+    return path
+
+  datatypedict = {
+    'bool' : BooleanType(),
+    'int' : IntegerType(),
+    'uint' : IntegerType(),
+    'long' : LongType(),
+    'ulong' : LongType(),
+    'float' : FloatType(),
+    'string' : StringType(),
+    'DateTime' : DateType(),
+  }
+
+  # return stream schema
+  def getSchema(self, streamName):
+    schema = StructType()
+    for field in self.streams[streamName][1]:
+      fieldname, fieldtype = field.split(':')
+      nullable = fieldtype.endswith('?')
+      if nullable:
+        fieldtype = fieldtype[:-1]
+      schema.add(StructField(fieldname, self.datatypedict[fieldtype], nullable))
+    return schema
+
+  # return stream dataframe
+  def getDataframe(self, streamName):
+    return spark.read.format('csv').options(header='false', delimiter='\t').schema(self.getSchema(streamName)).load(self.getFullpath(streamName))
+
+  # define stream dictionary
+  streams = {
+    'Authors' : ('authors.tsv', ['id:string', 'orcid:string?', 'display_name:string?', 'display_name_alternatives:string?', 'works_count:int?', 'cited_by_count:int?', 'last_known_institution:string?', 'works_api_url:string', 'updated_date:DateTime?']),
+    'AuthorsIds' : ('authors_ids.tsv', ['author_id:string', 'openalex:string?', 'orcid:string?', 'scopus:string?', 'twitter:string?', 'wikipedia:string?', 'mag:int?']),
+    'AuthorsCountsByYear' : ('authors_counts_by_year.tsv', ['author_id:string', 'year:int', 'works_count:int?', 'cited_by_count:int?']),
+    'Concepts' : ('concepts.tsv', ['id:string', 'wikidata:string?', 'display_name:string?', 'level:int?', 'description:string?', 'works_count:int?', 'cited_by_count:int?', 'image_url:string?', 'image_thumbnail_url:string?', 'works_api_url:string', 'updated_date:DateTime?']),
+    'ConceptsAncestors' : ('concepts_ancestors.tsv', ['concept_id:string', 'ancestor_id:string']),
+    'ConceptsCountsByYear' : ('concepts_counts_by_year.tsv', ['concept_id:string', 'year:int', 'works_count:int?', 'cited_by_count:int?']),
+    'ConceptsIds' : ('concepts_ids.tsv', ['concept_id:string', 'openalex:string?', 'wikidata:string?', 'wikipedia:string?', 'umls_aui:string?', 'umls_cui:string', 'mag:int?']),
+    'ConceptsRelatedConcepts' : ('concepts_related_concepts.tsv' , ['concept_id:string', 'related_concept_id:string', 'score:float?']),
+    'Institutions' : ('institutions.tsv', ['id:string', 'ror:string?', 'display_name:string?', 'country_code:string?', 'type:string?', 'homepage_url:string?', 'image_url:string?', 'image_thumbnail_url:string?', 'display_name_acroynyms:string?', 'display_name_alternatives:string?', 'works_count:int?', 'cited_by_count:int?', 'works_api_url:string', 'updated_date:DateTime?']),
+    'InstitutionsAssociatedInstitutions' : ('institutions_associated_institutions.tsv', ['institution_id:string?', 'associated_institution_id:string?', 'relationship:string?']),
+    'InstitutionsCountsByYear' : ('institutions_counts_by_year.tsv', ['institution_id:string', 'year:int', 'works_count:int?', 'cited_by_count:int?']),
+    'InstitutionsGeo' : ('institutions_geo.tsv', ['institution_id:string', 'city:string?', 'geonames_city_id:text?', 'region:string?', 'country_code:string?', 'country:string?', 'latitude:float?', 'longitude:float?']),
+    'InstitutionsIds' : ('institutions_ids.tsv', ['institution_id:string', 'openalex:string?', 'ror:string?', 'grid:string?', 'wikipedia:string?', 'wikidata:string?', 'mag:int?']),
+    'Works' : ('works.tsv', ['id:string', 'doi:string?', 'title:string?', 'display_name:string?', 'publication_year:int?', 'publication_date:string?', 'type:string?', 'cited_by_count:int?', 'is_retracted:bool?', 'is_paratext:bool?', 'host_venue:string?']),
+    'WorksAuthorships' : ('works_authorships.tsv', ['work_id:string?', 'author_position:string?', 'author_id:string?', 'institution_id:string?']),
+    'WorksAlternateHostVenues' : ('works_alternate_host_venues', ['work_id:string?', 'alternate_host_venue:string?']),
+    'WorksBiblio' : ('works_biblio.tsv', ['work_id:string', 'volume:string?', 'issue:string?', 'first_page:string?', 'last_page:string?']),
+    'WorksConcepts' : ('works_concepts.tsv', ['work_id:string?', 'concept_id:string?', 'score:float?']),
+    'WorksHostVenues' : ('works_host_venues.tsv', ['work_id:string?', 'venue_id:string?', 'url:string?', 'is_oa:bool?', 'version:string?', 'licence:string?']),
+    'WorksIds' : ('works_ids.tsv', ['work_id:string', 'openalex:string?', 'doi:string?', 'mag:int?', 'pmid:string?']),
+    'WorksMesh' : ('works_mesh.tsv', ['work_id:string?', 'descriptor_ui:string?', 'descriptor_name:string?', 'qualifier_ui:string?', 'qualifier_name:string?']),
+    'WorksOpenAccess' : ('works_open_access.tsv', ['work_id:string', 'is_oa:bool?', 'oa_status:string?', 'oa_url:string?']),
+    'WorksRelatedWorks': ('works_related_works.tsv', ['work_id:string?', 'related_work_id:string?']),
+    'WorksReferencedWorks' : ('works_referenced_works.tsv', ['work_id:string?', 'referenced_work_id:string?']),
+    'Venues' : ('venues.tsv', ['id:string', 'issn_l:string?', 'display_name:string?', 'publisher:string?', 'works_count:int?', 'cited_by_count:int?', 'is_oa:bool?', 'is_in_doaj:bool?', 'homepage_url:string?', 'works_api_url:string?', 'updated_date:DateTime?']),
+    'VenuesCountsByYear' : ('venues_counts_by_year.tsv', ['venue_id:string', 'year:int', 'works_count:int?', 'cited_by_count:int?']),
+    'VenuesIds' : ('venues_ids.tsv', ['venue_id:string', 'openalex:string?', 'issn_l:string?', 'issn:string?', 'mag:int?'])
+  }
+
+OA = OpenAlex()
+
+Authors = OA.getDataframe('Authors')
+AuthorsIds = OA.getDataframe('AuthorsIds')
+AuthorsCountsByYear = OA.getDataframe('AuthorsCountsByYear')
+Concepts = OA.getDataframe('Concepts')
+ConceptsAncestors = OA.getDataframe('ConceptsAncestors')
+ConceptsCountsByYear = OA.getDataframe('ConceptsCountsByYear')
+ConceptsIds = OA.getDataframe('ConceptsIds')
+ConceptsRelatedConcepts = OA.getDataframe('ConceptsRelatedConcepts')
+Institutions = OA.getDataframe('Institutions')
+InstitutionsAssociatedInstitutions = OA.getDataframe('InstitutionsAssociatedInstitutions')
+InstitutionsCountsByYear = OA.getDataframe('InstitutionsCountsByYear')
+InstitutionsGeo = OA.getDataframe('InstitutionsGeo')
+InstitutionsIds = OA.getDataframe('InstitutionsIds')
+Works = OA.getDataframe('Works')
+WorksAuthorships = OA.getDataframe('WorksAuthorships')
+WorksAlternateHostVenues = OA.getDataframe('WorksAlternateHostVenues')
+WorksBiblio = OA.getDataframe('WorksBiblio')
+WorksConcepts = OA.getDataframe('WorksConcepts')
+WorksHostVenues = OA.getDataframe('WorksHostVenues')
+WorksIds = OA.getDataframe('WorksIds')
+WorksMesh = OA.getDataframe('WorksMesh')
+WorksOpenAccess = OA.getDataframe('WorksOpenAccess')
+WorksRelatedWorks = OA.getDataframe('WorksRelatedWorks')
+WorksReferencedWorks = OA.getDataframe('WorksReferencedWorks')
+Venues = OA.getDataframe('Venues')
+VenuesCountsByYear = OA.getDataframe('VenuesCountsByYear')
+VenuesIds = OA.getDataframe('VenuesIds')

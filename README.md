89b1f6e3d425a8a43e672cdf3663d247d9554df7
MarioZZJ <mariozzj@outlook.com>
2022-11-12 16:19:24 +0800

    refactor: update data type
    * change ids'types to int;
    * add arraytype conversion
    * minor: fix typo

--- README.md
+++ README.md
@@ -2,9 +2,26 @@
 
 实验室服务器已完成 OpenAlex 数据集下载及转换，过程依据[官方文档](https://docs.openalex.org/download-snapshot)。本仓库包含基于 PySpark 的 OpenAlex 读取初始化过程，可方便基于该数据集进行实验。同时可以参考仓库内的 [教程 notebook](https://github.com/whuscity/pySparkDemo_OpenAlex/blob/main/OpenAlexSparkTutorial.ipynb) 进行基本学习。
 
+## 维护说明
+
+### ID 转换
+
+参照 [配置过程](https://blog.mariozzj.cn/posts/fb1f3a84/) 进行。需要特别说明：由于原始数据集中所有实体（Works、Authors、Concepts、Institutions、Venues）的 id 均为完整 url，形如 `http://openalex.org/works/W10000001`，会大大降低连接、聚合运算、匹配的计算速度，因此在维护中将其转换为纯数字 id，形如 `10000001`。如需取回原始 id，可以参考各实体对应的 ids 表 `OpenAlex` 字段。
+
+### 序列转换
+
+部分字段实际为序列，但转换时以字符串储存在文件中，因此对这些字段追加序列字段便于操作，追加的字段数据类型为 `pySpark.sql.types.ArrayType`：
+* `Authors` 表 `display_name_alternatives` 追加 `display_name_alternatives_array` 字段。
+* `ConceptsIds` 表 `umls_aui` 追加 `umls_aui_array` 字段。
+* `ConceptsIds` 表 `umls_cui` 追加 `umls_cui_array` 字段。
+* `Institutions` 表 `display_name_acronyms` 追加 `display_name_acronyms_array` 字段。
+* `Institutions` 表 `display_name_alternatives` 追加 `display_name_alternatives_array` 字段。
+* `Venues` 表 `issn` 追加 `issn_array` 字段。
+* `VenuesIds` 表 `issn` 追加 `issn_array` 字段。
+
 ## 使用方法
 
 本仓库已设置为 Github 模板仓库，可以基于 demo 创建仓库。步骤：
 1. 点击页面上方 `Use This Template`，创建自己的仓库；
 2. 使用 `git clone` 命令将自己的仓库克隆到到服务器上自己的文件夹内；
-3. 参照 demo，在服务器上创建自己的 notebook，进行自己的实验。注意关键代码块：`%run spark_openalex_init.py`
+3. 参照 [教程 notebook](https://github.com/whuscity/pySparkDemo_OpenAlex/blob/main/OpenAlexSparkTutorial.ipynb)，在服务器上创建自己的 notebook，进行自己的实验。注意关键代码块：`%run spark_openalex_init.py --appname=UserName_OpenAlex`
--- spark_openalex_init.py
+++ spark_openalex_init.py
@@ -9,7 +9,7 @@ import getopt
 class OpenAlex():
   # return stream path
   def getFullpath(self, streamName):
-    path = r'/home/Public/OpenAlex/csv-files/' + self.streams[streamName][0]
+    path = r'/home/Public/OpenAlex/tsv-files/' + self.streams[streamName][0]
     return path
 
   datatypedict = {
@@ -40,33 +40,33 @@ class OpenAlex():
 
   # define stream dictionary
   streams = {
-    'Authors' : ('authors.tsv', ['id:string', 'orcid:string?', 'display_name:string?', 'display_name_alternatives:string?', 'works_count:int?', 'cited_by_count:int?', 'last_known_institution:string?', 'works_api_url:string', 'updated_date:DateTime?']),
-    'AuthorsIds' : ('authors_ids.tsv', ['author_id:string', 'openalex:string?', 'orcid:string?', 'scopus:string?', 'twitter:string?', 'wikipedia:string?', 'mag:string?']),
-    'AuthorsCountsByYear' : ('authors_counts_by_year.tsv', ['author_id:string', 'year:int', 'works_count:int?', 'cited_by_count:int?']),
-    'Concepts' : ('concepts.tsv', ['id:string', 'wikidata:string?', 'display_name:string?', 'level:int?', 'description:string?', 'works_count:int?', 'cited_by_count:int?', 'image_url:string?', 'image_thumbnail_url:string?', 'works_api_url:string', 'updated_date:DateTime?']),
-    'ConceptsAncestors' : ('concepts_ancestors.tsv', ['concept_id:string', 'ancestor_id:string']),
-    'ConceptsCountsByYear' : ('concepts_counts_by_year.tsv', ['concept_id:string', 'year:int', 'works_count:int?', 'cited_by_count:int?']),
-    'ConceptsIds' : ('concepts_ids.tsv', ['concept_id:string', 'openalex:string?', 'wikidata:string?', 'wikipedia:string?', 'umls_aui:string?', 'umls_cui:string', 'mag:string?']),
-    'ConceptsRelatedConcepts' : ('concepts_related_concepts.tsv' , ['concept_id:string', 'related_concept_id:string', 'score:float?']),
-    'Institutions' : ('institutions.tsv', ['id:string', 'ror:string?', 'display_name:string?', 'country_code:string?', 'type:string?', 'homepage_url:string?', 'image_url:string?', 'image_thumbnail_url:string?', 'display_name_acroynyms:string?', 'display_name_alternatives:string?', 'works_count:int?', 'cited_by_count:int?', 'works_api_url:string', 'updated_date:DateTime?']),
-    'InstitutionsAssociatedInstitutions' : ('institutions_associated_institutions.tsv', ['institution_id:string?', 'associated_institution_id:string?', 'relationship:string?']),
-    'InstitutionsCountsByYear' : ('institutions_counts_by_year.tsv', ['institution_id:string', 'year:int', 'works_count:int?', 'cited_by_count:int?']),
-    'InstitutionsGeo' : ('institutions_geo.tsv', ['institution_id:string', 'city:string?', 'geonames_city_id:string?', 'region:string?', 'country_code:string?', 'country:string?', 'latitude:float?', 'longitude:float?']),
-    'InstitutionsIds' : ('institutions_ids.tsv', ['institution_id:string', 'openalex:string?', 'ror:string?', 'grid:string?', 'wikipedia:string?', 'wikidata:string?', 'mag:string?']),
-    'Works' : ('works.tsv', ['id:string', 'doi:string?', 'title:string?', 'display_name:string?', 'publication_year:int?', 'publication_date:string?', 'type:string?', 'cited_by_count:int?', 'is_retracted:bool?', 'is_paratext:bool?', 'host_venue:string?']),
-    'WorksAuthorships' : ('works_authorships.tsv', ['work_id:string?', 'author_position:string?', 'author_id:string?', 'institution_id:string?']),
-    'WorksAlternateHostVenues' : ('works_alternate_host_venues.tsv', ['work_id:string?', 'alternate_host_venue:string?']),
-    'WorksBiblio' : ('works_biblio.tsv', ['work_id:string', 'volume:string?', 'issue:string?', 'first_page:string?', 'last_page:string?']),
-    'WorksConcepts' : ('works_concepts.tsv', ['work_id:string?', 'concept_id:string?', 'score:float?']),
-    'WorksHostVenues' : ('works_host_venues.tsv', ['work_id:string?', 'venue_id:string?', 'url:string?', 'is_oa:bool?', 'version:string?', 'licence:string?']),
-    'WorksIds' : ('works_ids.tsv', ['work_id:string', 'openalex:string?', 'doi:string?', 'mag:string?', 'pmid:string?']),
-    'WorksMesh' : ('works_mesh.tsv', ['work_id:string?', 'descriptor_ui:string?', 'descriptor_name:string?', 'qualifier_ui:string?', 'qualifier_name:string?']),
-    'WorksOpenAccess' : ('works_open_access.tsv', ['work_id:string', 'is_oa:bool?', 'oa_status:string?', 'oa_url:string?']),
-    'WorksRelatedWorks': ('works_related_works.tsv', ['work_id:string?', 'related_work_id:string?']),
-    'WorksReferencedWorks' : ('works_referenced_works.tsv', ['work_id:string?', 'referenced_work_id:string?']),
-    'Venues' : ('venues.tsv', ['id:string', 'issn_l:string?', 'display_name:string?', 'publisher:string?', 'works_count:int?', 'cited_by_count:int?', 'is_oa:bool?', 'is_in_doaj:bool?', 'homepage_url:string?', 'works_api_url:string?', 'updated_date:DateTime?']),
-    'VenuesCountsByYear' : ('venues_counts_by_year.tsv', ['venue_id:string', 'year:int', 'works_count:int?', 'cited_by_count:int?']),
-    'VenuesIds' : ('venues_ids.tsv', ['venue_id:string', 'openalex:string?', 'issn_l:string?', 'issn:string?', 'mag:string?'])
+    'Authors' : ('authors.tsv', ['id:int', 'orcid:string?', 'display_name:string?', 'display_name_alternatives:string?', 'works_count:int?', 'cited_by_count:int?', 'last_known_institution:string?', 'works_api_url:string', 'updated_date:DateTime?']),
+    'AuthorsIds' : ('authors_ids.tsv', ['author_id:int', 'openalex:string?', 'orcid:string?', 'scopus:string?', 'twitter:string?', 'wikipedia:string?', 'mag:int?']),
+    'AuthorsCountsByYear' : ('authors_counts_by_year.tsv', ['author_id:int', 'year:int', 'works_count:int?', 'cited_by_count:int?']),
+    'Concepts' : ('concepts.tsv', ['id:int', 'wikidata:string?', 'display_name:string?', 'level:int?', 'description:string?', 'works_count:int?', 'cited_by_count:int?', 'image_url:string?', 'image_thumbnail_url:string?', 'works_api_url:string', 'updated_date:DateTime?']),
+    'ConceptsAncestors' : ('concepts_ancestors.tsv', ['concept_id:int', 'ancestor_id:string']),
+    'ConceptsCountsByYear' : ('concepts_counts_by_year.tsv', ['concept_id:int', 'year:int', 'works_count:int?', 'cited_by_count:int?']),
+    'ConceptsIds' : ('concepts_ids.tsv', ['concept_id:int', 'openalex:string?', 'wikidata:string?', 'wikipedia:string?', 'umls_aui:string?', 'umls_cui:string', 'mag:int?']),
+    'ConceptsRelatedConcepts' : ('concepts_related_concepts.tsv' , ['concept_id:int', 'related_concept_id:int', 'score:float?']),
+    'Institutions' : ('institutions.tsv', ['id:int', 'ror:string?', 'display_name:string?', 'country_code:string?', 'type:string?', 'homepage_url:string?', 'image_url:string?', 'image_thumbnail_url:string?', 'display_name_acronyms:string?', 'display_name_alternatives:string?', 'works_count:int?', 'cited_by_count:int?', 'works_api_url:string', 'updated_date:DateTime?']),
+    'InstitutionsAssociatedInstitutions' : ('institutions_associated_institutions.tsv', ['institution_id:int', 'associated_institution_id:int', 'relationship:string?']),
+    'InstitutionsCountsByYear' : ('institutions_counts_by_year.tsv', ['institution_id:int', 'year:int', 'works_count:int?', 'cited_by_count:int?']),
+    'InstitutionsGeo' : ('institutions_geo.tsv', ['institution_id:int', 'city:string?', 'geonames_city_id:text?', 'region:string?', 'country_code:string?', 'country:string?', 'latitude:float?', 'longitude:float?']),
+    'InstitutionsIds' : ('institutions_ids.tsv', ['institution_id:int', 'openalex:string?', 'ror:string?', 'grid:string?', 'wikipedia:string?', 'wikidata:string?', 'mag:int?']),
+    'Works' : ('works.tsv', ['id:int', 'doi:string?', 'title:string?', 'display_name:string?', 'publication_year:int?', 'publication_date:string?', 'type:string?', 'cited_by_count:int?', 'is_retracted:bool?', 'is_paratext:bool?', 'host_venue:string?']),
+    'WorksAuthorships' : ('works_authorships.tsv', ['work_id:int', 'author_position:string?', 'author_id:string?', 'institution_id:string?']),
+    'WorksAlternateHostVenues' : ('works_alternate_host_venues', ['work_id:int', 'alternate_host_venue:int']),
+    'WorksBiblio' : ('works_biblio.tsv', ['work_id:int', 'volume:string?', 'issue:string?', 'first_page:string?', 'last_page:string?']),
+    'WorksConcepts' : ('works_concepts.tsv', ['work_id:int', 'concept_id:int', 'score:float?']),
+    'WorksHostVenues' : ('works_host_venues.tsv', ['work_id:int', 'venue_id:int', 'url:string?', 'is_oa:bool?', 'version:string?', 'licence:string?']),
+    'WorksIds' : ('works_ids.tsv', ['work_id:int', 'openalex:string?', 'doi:string?', 'mag:int?', 'pmid:string?']),
+    'WorksMesh' : ('works_mesh.tsv', ['work_id:int', 'descriptor_ui:string?', 'descriptor_name:string?', 'qualifier_ui:string?', 'qualifier_name:string?']),
+    'WorksOpenAccess' : ('works_open_access.tsv', ['work_id:int', 'is_oa:bool?', 'oa_status:string?', 'oa_url:string?']),
+    'WorksRelatedWorks': ('works_related_works.tsv', ['work_id:int', 'related_work_id:int']),
+    'WorksReferencedWorks' : ('works_referenced_works.tsv', ['work_id:int', 'referenced_work_id:int']),
+    'Venues' : ('venues.tsv', ['id:int', 'issn_l:string?', 'issn:string?', 'display_name:string?', 'publisher:string?', 'works_count:int?', 'cited_by_count:int?', 'is_oa:bool?', 'is_in_doaj:bool?', 'homepage_url:string?', 'works_api_url:string?', 'updated_date:DateTime?']),
+    'VenuesCountsByYear' : ('venues_counts_by_year.tsv', ['venue_id:int', 'year:int', 'works_count:int?', 'cited_by_count:int?']),
+    'VenuesIds' : ('venues_ids.tsv', ['venue_id:int', 'openalex:string?', 'issn_l:string?', 'issn:string?', 'mag:int?'])
   }
 
 if __name__ == '__main__':
@@ -135,6 +135,9 @@ if __name__ == '__main__':
 
 
     Authors = Authors.withColumn("display_name_alternatives_array", to_array(Authors.display_name_alternatives))
+    ConceptsIds = ConceptsIds.withColumn("umls_aui_array", to_array(ConceptsIds.umls_aui))
+    ConceptsIds = ConceptsIds.withColumn("umls_cui_array", to_array(ConceptsIds.umls_cui))
+    Institutions = Institutions.withColumn("display_name_acronyms_array", to_array(Institutions.display_name_acronyms))
     Institutions = Institutions.withColumn("display_name_alternatives_array", to_array(Institutions.display_name_alternatives))
-    Venues = Venues.withColumn("display_name_array", to_array(Venues.display_name))
+    Venues = Venues.withColumn("issn_array", to_array(Venues.issn))
     VenuesIds = VenuesIds.withColumn("issn_array", to_array(VenuesIds.issn))
